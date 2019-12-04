5e46db33c1ff64e80fc8679d965d996162e1e0d0
jdromero88 <github email address>
2019-12-04 14:16:58 -0500

    item CRUD working

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,30 +1,40 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all
   end
 
   def new
-
+    @item = Item.new
   end
 
   def create
-
+    @item = Item.create(items_strong_params)
+    redirect_to item_path(@item)
   end
 
   def show
-
   end
 
   def edit
-
   end
 
   def update
-
+    @item.update(items_strong_params)
+    redirect_to item_path(@item)
   end
 
   def destroy
+    @item.destroy
+    redirect_to items_path
   end
 
+  private
+    def find_item
+      @item = Item.find(params[:id])
+    end
+    def items_strong_params
+      params.require(:item).permit(:name, :price, :category_id)
+    end
 end
