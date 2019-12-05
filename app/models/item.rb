bafa75d4762c028119d43fd1b51de9a4cd8d63c1
jdromero88 <github email address>
2019-12-05 00:56:32 -0500

    Fixed item pages, the way we render the partial pages

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,6 @@ class Item < ApplicationRecord
   belongs_to :category
 
   validates :name, presence: true, uniqueness: true
+  validates :price, presence: true
+  validates :category_id, presence: true
 end
